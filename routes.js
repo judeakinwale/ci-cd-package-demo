26f730178b86bb8168fcd359bfc6f166ddbda3f7
judeakinwale <judeakinwale@gmail.com>
2022-10-06 09:48:10 +0100

    Minor Changes

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).send("Welcome to the application!")
+    res.status(200).send("Welcome!")
 })
 
 app.get('/test', (req, res) => {
